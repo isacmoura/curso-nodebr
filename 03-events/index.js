be617007f830d4e54729365ebf83b9450cd9308c
Isac Moura <contatoisacmoura@outlook.com>
2019-02-04 15:44:40 -0300

    Events em JS

---
+++ 03-events/index.js
@@ -0,0 +1,33 @@
+const eventMitter = require('events')
+
+class meuEmissor extends eventMitter {
+
+}
+
+meuEmissor = new meuEmissor()
+const nomeEvento = 'usuario:click'
+meuEmissor.on(nomeEvento, function(click) {
+    console.log('Um usuário clicou', click)
+})
+
+// meuEmissor.emit(nomeEvento, 'na barra de rolagem')
+// meuEmissor.emit(nomeEvento, 'no ok')
+
+// let count = 0
+// setInterval(function(){
+//     meuEmissor.emit(nomeEvento, 'no ok' + (count++))
+// }, 1000)
+
+const stdin = process.openStdin()
+function main(){
+    return new Promise(function(resolve, reject) {
+        stdin.addListener('data', function(value) {
+            //console.log(`Você digitou ${value.toString().trim()}`)
+            return resolve(value)
+        })
+    })
+}
+
+main().then(function(resultado) {
+    console.log('resultado', resultado.toString())
+})
